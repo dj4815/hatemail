1985526d6cae7c89aced1c324e9cc4545b4aa356
Dom <86100131+dj4815@users.noreply.github.com>
2024-11-24 15:39:44 +0100

    hate

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const app = document.querySelector("#app");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const hatemail = document.querySelector("input");
-  console.log(hatemail.value);
   const p = document.createElement("p");
+  const randomColor = makeRandomColor();
+
   app.append(p);
+
   p.innerText = hatemail.value;
+  p.style.backgroundColor = randomColor;
+
   hatemail.value = "";
 });
+
+const makeRandomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
